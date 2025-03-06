5b66ab7c6ad4a1f0443d4763a98a5f6da5558499
HusseinMassalkhi55 <halmassalkhi@sabis.net>
2025-03-06 12:02:26 +0000

    Update

--- your-scratch-extension/index.js
+++ your-scratch-extension/index.js
@@ -1,66 +1,50 @@
 const BlockType = require('../../extension-support/block-type');
 const ArgumentType = require('../../extension-support/argument-type');
 const TargetType = require('../../extension-support/target-type');
+const syllable = require('syllable');
 
 class Scratch3YourExtension {
 
   constructor (runtime) {
-    import('syllable')
-      .then((syllableModule) => {
-        this.syllable = syllableModule.syllable;
-      });
+    this.syllable = syllable;
   }
 
   getInfo () {
     return {
-      // unique ID for your extension
       id: 'yourScratchExtension',
-  
-      // name displayed in the Scratch UI
       name: 'Demo',
-  
-      // colours to use for your extension blocks
       color1: '#000099',
       color2: '#660066',
-  
-      // your Scratch blocks
       blocks: [
         {
-          // function where your code logic lives
           opcode: 'myFirstBlock',
-  
-          // type of block
           blockType: BlockType.REPORTER,
-  
-          // label to display on the block
           text: 'Title for ISBN book [BOOK_NUMBER]',
-  
-          // arguments used in the block
           arguments: {
             BOOK_NUMBER: {
               defaultValue: 1718500564,
-  
-              // type/shape of the parameter
               type: ArgumentType.NUMBER
             }
           }
         },
         {
-          // function where your code logic lives
           opcode: 'mySecondBlock',
-  
-          // type of block
           blockType: BlockType.REPORTER,
-  
-          // label to display on the block
           text: 'Syllables in [MY_TEXT]',
-  
-          // arguments used in the block
           arguments: {
             MY_TEXT: {
               defaultValue: 'Hello World',
-  
-              // type/shape of the parameter
+              type: ArgumentType.STRING
+            }
+          }
+        },
+        {
+          opcode: 'getWeather',
+          blockType: BlockType.REPORTER,
+          text: 'Current weather in [CITY]',
+          arguments: {
+            CITY: {
+              defaultValue: 'Beirut',
               type: ArgumentType.STRING
             }
           }
@@ -69,27 +53,48 @@ class Scratch3YourExtension {
     };
   }
 
-      myFirstBlock ({ BOOK_NUMBER }) {
-        return fetch('https://openlibrary.org/isbn/' + BOOK_NUMBER + '.json')
-          .then((response) => {
-            if (response.ok) {
-              return response.json();
-            }
-            else {
-              return { title: 'Unknown' };
-            }
-          })
-          .then((bookinfo) => {
-            return bookinfo.title;
-          });
-      }
-      mySecondBlock ({ MY_TEXT }) {
-        return this.syllable(MY_TEXT);
-      }
+  myFirstBlock ({ BOOK_NUMBER }) {
+    return fetch('https://openlibrary.org/isbn/' + BOOK_NUMBER + '.json')
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          return { title: 'Unknown' };
+        }
+      })
+      .then((bookinfo) => {
+        return bookinfo.title;
+      })
+      .catch((error) => {
+        console.error('Error fetching book info:', error);
+        return 'Error fetching title';
+      });
+  }
 
+  mySecondBlock ({ MY_TEXT }) {
+    return this.syllable(MY_TEXT);
+  }
+
+  getWeather({ CITY }) {
+    const apiKey = 'f56dd4b445458e09f03bbe3ccd5a007f';
+    const url = `https://api.openweathermap.org/data/3.0/onecall?lat=33.8938&lon=35.5018&exclude=hourly,daily&appid=${apiKey}&units=metric`;
+
+    return fetch(url)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          return { weather: 'Unknown' };
+        }
+      })
+      .then((weatherData) => {
+        return `Temperature: ${weatherData.current.temp}°C, ${weatherData.current.weather[0].description}`;
+      })
+      .catch((error) => {
+        console.error('Error fetching weather data:', error);
+        return 'Error fetching weather';
+      });
+  }
 }
 
 module.exports = Scratch3YourExtension;
-
-
-
